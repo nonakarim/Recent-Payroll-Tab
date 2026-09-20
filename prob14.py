36511cbd7b712ae0933275d5d028d650be2d1ea5
Hana Karim <nona.karim25@gmail.com>
2026-09-20 14:37:03 +0300

    Challenge, Functions, and Comments done

--- prob14.py
+++ prob14.py
@@ -1,9 +1,31 @@
 import streamlit as st
 import time
 
+# Creating session state variables
+
 if "payroll_records" not in st.session_state:
     st.session_state.payroll_records = []
 
+if "total_salaries" not in st.session_state:
+    st.session_state.total_salaries = 0
+
+if "high_salary" not in st.session_state:
+    st.session_state.high_salary = 0
+
+if "low_salary" not in st.session_state:
+    st.session_state.low_salary = 0
+
+
+if "high_overtme" not in st.session_state:
+    st.session_state.high_overtme = 0
+
+if "total_bonus" not in st.session_state:
+    st.session_state.total_bonus = 0
+
+if "total_deduction" not in st.session_state:
+    st.session_state.total_deduction = 0
+
+# Employee dictionary
 employees = [
     {"id": 1001, "name": "Ahmed", "hourly_rate": 120},
     {"id": 1002, "name": "Sara", "hourly_rate": 150},
@@ -11,22 +33,24 @@ employees = [
     {"id": 1004, "name": "Mariam", "hourly_rate": 200}
 ]
 
-st.title("Recent Payroll Tab", text_alignment="center")
 
-st.divider()
-
-with st.sidebar:
+def side_bar():
+    """Prints Details on Sidebar"""
     st.header("Details")
+
+    # Salary Calculation Formulas
     st.subheader("Salary Calculation")
     st.info("Basic Salary = Hourly Rate × Working Hours")
     st.info("OT Pay = Hourly Rate × Overtime Hours × 1.5")
     st.info("Gross Salary = Basic Salary + OT Pay + Bonus")
     st.info("Net Salary = Gross Salary − Deduction")
 
+    # Company Working Hours 
     st.subheader("Company Working Hours")
     st.markdown("""- Standard workday: 8 hours""")
     st.markdown("""- Standard workweek: 40 hours""")
 
+    # Payroll Policy
     st.subheader("Payroll Policy")
     st.markdown("""
                 - Basic salary = Hourly rate × Working hours
@@ -37,9 +61,11 @@ with st.sidebar:
                 - Overtime above 20 hours triggers a warning
                 """)
 
-tab1, tab2, tab3 = st.tabs(["Payroll Calculator", "Recent Payroll", "All Employees Payroll"])
 
-with tab1:
+def tab_1():
+    """Tab1 Code"""
+
+    # Input Form
     with st.form("Payroll Inputs"):
         employee = st.selectbox("👤Select Employee: ", employees, format_func=lambda e: f"{e['id']} - {e['name']}")
         employee_working_hours = st.number_input("🖥️Enter Employee Working Hours: ", min_value=1)
@@ -48,18 +74,23 @@ with tab1:
         deduction = st.number_input("📉Enter Employee Deduction: ", min_value=0)
         submitted = st.form_submit_button("Calculate Payroll")
 
+        # Too many Overtime Hours WARNING
         if submitted and employee_overtime_hours > 20:
-            st.spinner("")
             st.warning("Overtime Hours are TOO LONG!!!")
+
         if submitted:
+            # Still Downloading
             with st.spinner(""):
                 time.sleep(2)
+
+            # Setting Salary Values
             hourly_rate = employee["hourly_rate"]
             basic_salary = hourly_rate * employee_working_hours
             OT_pay = hourly_rate * employee_overtime_hours * 1.5
             gross_salary = basic_salary + OT_pay + bonus
             net_salary = gross_salary - deduction
 
+            # Printing salaries
             st.markdown(f"""
                         - Basic salary = {basic_salary} EGP
                         - OT Pay = {OT_pay} EGP
@@ -67,8 +98,24 @@ with tab1:
                         - Net Salary = {net_salary} EGP
                         """)
 
+            # Settimg arguments needed for tab4
+            st.session_state.total_salaries += net_salary
+
+            if net_salary > st.session_state.high_salary:
+                st.session_state.high_salary = net_salary
+                        
+            if net_salary < st.session_state.low_salary:
+                st.session_state.low_salary = net_salary
+
+            st.session_state.total_bonus += bonus
+
+            st.session_state.total_deduction += deduction
+
+
+            # Declaring Performance
             performance = ""
 
+            # Setting employee performance
             if employee_overtime_hours == 0:
                 performance = "Standard"
             elif employee_overtime_hours >= 1 and employee_overtime_hours <= 10:
@@ -78,10 +125,12 @@ with tab1:
             else:
                 performance = "Outstanding"
 
+            # Printing employee performance
             st.markdown(f""" 
                         - **Performance:** {performance}
                     """)
 
+            # Initializing records dictionary
             st.session_state.payroll_records.append({
                                                     "ID": employee["id"],
                                                     "Name": employee["name"],
@@ -96,15 +145,18 @@ with tab1:
                                                     "Performance": performance
                                                 })
 
+            # Payroll Success Message
             st.success("Payroll Completed Successfully")
 
-with tab2:
+def tab_2():
+    """Code for tab2"""
     if st.session_state.payroll_records:
 
         record = st.session_state.payroll_records[-1]
 
         col1, col2 = st.columns([1, 1])
 
+        # Printing Last employee Deets
         with col1:
             st.markdown(f"""
             - ID: {record["ID"]}
@@ -124,11 +176,46 @@ with tab2:
             - **Performance:** {record["Performance"]}
             """)
 
+    # No records yet VALIDATION
     else:
         st.info("No payroll records yet. Calculate a payroll first.")
 
-with tab3:
+def tab_3():
+    """Prints Table with all Employees entered and their DEETS"""
     if st.session_state.payroll_records:
         st.table(st.session_state.payroll_records)
     else:
         st.info("No payroll records yet. Calculate a payroll first.")
+
+
+
+
+st.title("Recent Payroll Tab", text_alignment="center")
+
+st.divider()
+
+with st.sidebar:
+    side_bar()
+
+tab1, tab2, tab3, tab4 = st.tabs(["Payroll Calculator", "Recent Payroll", "All Employees Payroll", "Monthly Payroll Dashboard"])
+
+with tab1:
+    tab_1()
+
+with tab2:
+    tab_2()
+
+with tab3:
+    tab_3()
+
+with tab4:
+    if len(st.session_state.payroll_records) != 0:
+        st.markdown(f"""
+                    - Total employees processed: {len(st.session_state.payroll_records)}
+                    - Total Salaries Paid: {st.session_state.total_salaries}
+                    - Average Salary: {st.session_state.total_salaries/len(st.session_state.payroll_records)}
+                    - Highest Salary: {st.session_state.high_salary}
+                    - Lowest Salary: {st.session_state.low_salary}
+                    - Total Bonus: {st.session_state.total_bonus}
+                    - Total Deduction: {st.session_state.total_deduction}
+                    """)
